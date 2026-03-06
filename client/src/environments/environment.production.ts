fd9dc72e51d1de07948617485a932deee37951ce
mittalswati <mittalswati@gmail.com>
2026-03-05 16:17:59 -0800

    fix: use actual Azure API URL (canadacentral)

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://trackr-api.azurewebsites.net',
-  hubUrl: 'https://trackr-api.azurewebsites.net/hubs/location'
+  apiUrl: 'https://trackr-api-h0h9gbesb8e2gvhh.canadacentral-01.azurewebsites.net',
+  hubUrl: 'https://trackr-api-h0h9gbesb8e2gvhh.canadacentral-01.azurewebsites.net/hubs/location'
 };
